cce37e3c7c44d4177ec8de5344de0891dab7f716
ShujinZhong <rachelzhongshujin@gmail.com>
2019-09-29 17:28:29 -0400

    unzip the manully downloaded data

--- tensorflow_datasets/image/breastpathq.py
+++ tensorflow_datasets/image/breastpathq.py
@@ -14,11 +14,11 @@ import tensorflow_datasets as tfds
 _URL = "http://spiechallenges.cloudapp.net/competitions/14#participate"
 
 # BibTeX citation
-_CITATION = """\
+_CITATION = """
 @article{peikari2017automatic,
-  title={Automatic cellularity assessment from \
+  title={Automatic cellularity assessment from
   post-treated breast surgical specimens},
-  author={Peikari, Mohammad and Salama, Sherine and \
+  author={Peikari, Mohammad and Salama, Sherine and
   Nofech-Mozes, Sharon and Martel, Anne L},
   journal={Cytometry Part A},
   volume={91},
@@ -30,93 +30,101 @@ _CITATION = """\
 """
 
 
-_DESCRIPTION = """\
-The dataset's training/validation set consists of \
-2578 patches extracted from 96 breast cancer \
-whole slide images (WSI). Each patch is labelled \
-by a tumor cellularity score. The testing set \
-contains 1121 patches from 25 WSIs. Labels for \
-testing data are not provided by far. \
-The dataset can be used to develop an automated method \
-for evaluating cancer cellularity from \
-histology patches extracted from WSIs. The method \
-is aimed to increase reproducibility of cancer \
+_DESCRIPTION = """
+The dataset's training/validation set consists of 2578 patches extracted from
+96 breast cancer whole slide images (WSI). Each patch is labelled by a tumor
+cellularity score. The testing set contains 1121 patches from 25 WSIs. Labels
+for testing data are not provided by far. The dataset can be used to develop
+an automated method for evaluating cancer cellularity from histology patches
+extracted from WSIs. The method is aimed to increase reproducibility of cancer
 cellularity scores and enhance tumor burden assessment.
 """
 
 _IMAGE_SHAPE = (512, 512, 3)
 
+
 def _load_tif(path):
-  with tf.io.gfile.GFile(path, "rb") as fp:
-    image = tfds.core.lazy_imports.PIL_Image.open(fp)
-    rgb_img = image.convert("RGB")
-  return np.array(rgb_img)
+    with tf.io.gfile.GFile(path, "rb") as fp:
+        image = tfds.core.lazy_imports.PIL_Image.open(fp)
+        rgb_img = image.convert("RGB")
+    return np.array(rgb_img)
 
 
 class Breastpathq(tfds.core.GeneratorBasedBuilder):
-  """Breast cancer whole slide image dataset."""
+    """Breast cancer whole slide image dataset."""
 
-  # Set up version.
-  VERSION = tfds.core.Version('0.1.0')
+    # Set up version.
+    VERSION = tfds.core.Version('0.1.0')
 
-  def _info(self):
-    # Specifies the tfds.core.DatasetInfo object
-    return tfds.core.DatasetInfo(
-        builder=self,
-        # This is the description that will appear on the datasets page.
-        description=_DESCRIPTION,
-        # tfds.features.FeatureConnectors
-        features=tfds.features.FeaturesDict({
-            # These are the features of your dataset like images, labels ...
-            "image": tfds.features.Image(),
-            "label": tf.float32
-        }),
-        # If there's a common (input, target) tuple from the features,
-        # specify them here. They'll be used if as_supervised=True in
-        # builder.as_dataset.
-        supervised_keys=("image", "label"),
-        # Homepage of the dataset for documentation
-        urls=[_URL],
-        citation=_CITATION,
-    )
+    def _info(self):
+        # Specifies the tfds.core.DatasetInfo object
+        return tfds.core.DatasetInfo(
+            builder=self,
+            # This is the description that will appear on the datasets page.
+            description=_DESCRIPTION,
+            # tfds.features.FeatureConnectors
+            features=tfds.features.FeaturesDict({
+                # These are the features of your dataset like images, labels ..
+                "image": tfds.features.Image(),
+                "label": tf.float32
+                }),
+            # If there's a common (input, target) tuple from the features,
+            # specify them here. They'll be used if as_supervised = True in
+            # builder.as_dataset.
+            supervised_keys=("image", "label"),
+            # Homepage of the dataset for documentation
+            urls=[_URL],
+            citation=_CITATION,
+            )
 
-  def _split_generators(self, dl_manager):
-    """Returns SplitGenerators."""
-    # Downloads the data and defines the splits
-    # dl_manager is a tfds.download.DownloadManager that can be used to
-    # download and extract URLs
-    # manual download is required for this dataset
-    extracted_path = dl_manager.manual_dir
-    return [
-        tfds.core.SplitGenerator(
-            name=tfds.Split.TRAIN,
-            # These kwargs will be passed to _generate_examples
-            gen_kwargs={
-              "images_dir_path": os.path.join(extracted_path, \
-                "breastpathq/datasets/train"),
-              "labels": os.path.join(extracted_path, \
-                "breastpathq/datasets/train_labels.csv"),
-            },
-        ),
-        tfds.core.SplitGenerator(
-          name=tfds.Split.VALIDATION,
-          gen_kwargs={
-            "images_dir_path": os.path.join(extracted_path, \
-              "breastpathq/datasets/validation"),
-            "labels": os.path.join(extracted_path, \
-              "breastpathq-test/val_labels.csv"),
-          }
-        ),
-    ]
+    def _split_generators(self, dl_manager):
+        """Returns SplitGenerators."""
+        # Downloads the data and defines the splits
+        # dl_manager is a tfds.download.DownloadManager that can be used to
+        # download and extract URLs
+        # manual download is required for this dataset
+        download_path = dl_manager.manual_dir
+        extracted_path = dl_manager.extract(download_path)
+        return [
+            tfds.core.SplitGenerator(
+                name=tfds.Split.TRAIN,
+                # These kwargs will be passed to _generate_examples
+                gen_kwargs={
+                    "images_dir_path": os.path.join(
+                        extracted_path,
+                        "breastpathq/datasets/train"
+                        ),
+                    "labels": os.path.join(
+                        extracted_path,
+                        "breastpathq/datasets/train_labels.csv"
+                        ),
+                    },
+                ),
+            tfds.core.SplitGenerator(
+                name=tfds.Split.VALIDATION,
+                gen_kwargs={
+                    "images_dir_path":
+                    os.path.join(
+                        extracted_path,
+                        "breastpathq/datasets/validation"
+                        ),
+                    "labels": os.path.join(
+                        extracted_path,
+                        "breastpathq-test/val_labels.csv"
+                        ),
+                    }
+                ),
+            ]
 
-  def _generate_examples(self, images_dir_path, labels):
-    """Yields examples."""
-    # Yields (key, example) tuples from the dataset
-    with tf.io.gfile.GFile(labels, "r") as f:
-      dataset = csv.DictReader(f)
-      for row in dataset:
-        image_id = row['slide']+'_'+row['rid']
-        yield image_id, {
-            "image": _load_tif(os.path.join(images_dir_path, image_id+'.tif')),
-            'label': row['y'],
-        }
+    def _generate_examples(self, images_dir_path, labels):
+        """Yields examples."""
+        # Yields (key, example) tuples from the dataset
+        with tf.io.gfile.GFile(labels, "r") as f:
+            dataset = csv.DictReader(f)
+            for row in dataset:
+                image_id = row['slide']+'_'+row['rid']
+                yield image_id, {
+                    "image": _load_tif(os.path.join(images_dir_path,
+                                                    image_id+'.tif')),
+                    'label': row['y'],
+                }
